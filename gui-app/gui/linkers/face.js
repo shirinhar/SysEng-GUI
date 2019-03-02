9800122a9b6c6130dc4ca64ab4a38febc3a25de3
shirinhar <shirin.harandi.17@ucl.ac.uk>
2019-03-02 11:02:31 +0000

    simple camera view

--- gui-app/gui/linkers/face.js
+++ gui-app/gui/linkers/face.js
@@ -5,16 +5,22 @@ function detect_faces() {
   document.getElementById("detect").value = "Hang on..."
   var python = require("python-shell")
   var path = require("path")
+  var ps = require("python-shell")
 
     var options = {
     scriptPath : path.join(__dirname, '/../engine/'),
     pythonPath : '/usr/local/bin/python3'
   }
 
-  var face = new python("clientApplication.py", options);
+  // var face = new python("clientApplication.py", options);
 
-  face.end(function(err, code, message) {
-    document.getElementById("detect").value = "Detect faces";
-    })
+  ps.PythonShell.run('clientApplication.py', options, function(err, results) {
+    if (err) throw err;
+    // document.getElementById("detect").value = "Detect faces";
+  });
+
+  // face.end(function(err, code, message) {
+  //   document.getElementById("detect").value = "Detect faces";
+  //   })
 
 }
--- gui-app/gui/pages/login/login.js
+++ gui-app/gui/pages/login/login.js
@@ -1,25 +1,29 @@
-function field_focus(field, email)
-  {
+function field_focus(field, email) {
     if(field.value == email)
     {
       field.value = '';
     }
-  }
+}
 
-  function field_blur(field, email)
-  {
+function field_blur(field, email) {
     if(field.value == '')
     {
       field.value = email;
     }
-  }
+}
 
-//Fade in dashboard box
-$(document).ready(function(){
-    $('.box').hide().fadeIn(1000);
-    });
+function login() {
+  
+}
 
-//Stop click event
-$('a').click(function(event){
-    event.preventDefault();
-	});
+
+
+// //Fade in dashboard box
+// $(document).ready(function(){
+//     $('.box').hide().fadeIn(1000);
+//     });
+//
+// //Stop click event
+// $('a').click(function(event){
+//     event.preventDefault();
+// 	});
